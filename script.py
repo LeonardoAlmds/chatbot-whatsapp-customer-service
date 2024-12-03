a552f0fcded829ff37e0b040903bcc6e2cb1b2a3
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2024-12-03 12:31:35 -0300

    Enhance message handling in WhatsApp automation: track new messages and display them with timestamps

--- script.py
+++ script.py
@@ -40,7 +40,7 @@ try:
     )
     print("Logged in successfully")
 
-    contact_name = ""
+    contact_name = "+55 81 8994-5697"
     search_bar.send_keys(contact_name)
     search_bar.send_keys(Keys.ENTER)
 
@@ -61,31 +61,41 @@ try:
 
     print("Messages sent successfully")
     print("Reading received messages...")
+
+    last_message_count = len(driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text'))
+    
+    message_hour = driver.find_elements(By.CLASS_NAME, 'x1rg5ohu.x16dsc37')
+    last_hour = message_hour[-1].text
     
     while True:
         time.sleep(5)
 
         received_messages = driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text')
-        if received_messages:
-            last_message = received_messages[-1].text
-            
-            message_hour = driver.find_elements(By.CLASS_NAME, 'x1rg5ohu.x16dsc37')
-            last_hour = message_hour[-1].text
-                                            
-            print("Received message at " + last_hour)
-            print("Received message: " + last_message)
-            
-            if last_message == "1":
-                print("Option 1 selected")
-            elif last_message == "2":
-                print("Option 2 selected")
-            elif last_message == "3":
-                print("Option 3 selected")
-            elif last_message == "4":
-                print("Option 4 selected. Exiting...")
-                break
-            else:
-                print("Invalid option selected")
+        current_message_count = len(received_messages)
+
+        if current_message_count > last_message_count:
+            new_messages = received_messages[last_message_count:]
+            for message_element in new_messages:
+                last_message = message_element.text
+                print(f"New message received: {last_message}" + " at " + last_hour)
+
+                if last_message == "1":
+                    print("Option 1 selected")
+                elif last_message == "2":
+                    print("Option 2 selected")
+                elif last_message == "3":
+                    print("Option 3 selected")
+                elif last_message == "4":
+                    print("Option 4 selected. Exiting...")
+                    message = "Goodbye!"
+                    input_message.send_keys(message)
+                    input_message.send_keys(Keys.ENTER)
+                    time.sleep(10)
+                    break
+                else:
+                    print("Invalid option selected")
+
+            last_message_count = current_message_count
 
 except Exception as e:
     print(f"An error occurred: {e}")
