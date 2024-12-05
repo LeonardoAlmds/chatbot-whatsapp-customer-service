81371e9bfa45171fa3a531d555f9efb4fa275fac
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2024-12-04 21:26:15 -0300

    first commit

---
+++ vini.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:/caminho/para/pasta/de/perfil")
+
+browser = webdriver.Chrome(options=options)
+wait = WebDriverWait(browser, 10)
+browser.get("https://web.whatsapp.com/")
+print("Waiting for you scan your QRcode")
+sleep(10)   
+
+def openUnread():
+    unreadMessage = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[3]/div/div[2]/button[2]")
+    unreadMessage.click()
+
+def menu():
+    input_message = wait.until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[3]/div[4]/div/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p'))
+    )
+
+    messages = [
+        "Options:",
+        "",
+        "1 - SEE ALL PRODUCTS",
+        "2 - SEE ALL CATEGORIES",
+        "3 - SEE ALL BRANDS",
+        "4 - EXIT",
+        "",
+        "Type the number of the option you want to choose:"
+    ]
+
+    for message in messages:
+        input_message.send_keys(message)
+        input_message.send_keys(Keys.SHIFT, Keys.ENTER)
+
+    input_message.send_keys(Keys.ENTER)
+
+c = True
+while c:
+    try:
+        openUnread()
+        c = False
+    except Exception as e:
+        print(f"error {e}, trying again")
+        openUnread()
+i = 0
+message = 0
+while True:
+    sleep(2)
+    try:
+        bubbleNotifications = browser.find_elements(By.CLASS_NAME, "_ahlk")
+        for i in range(len(bubbleNotifications)):
+            notification = bubbleNotifications[i]
+            notification.click()
+        sleep(2)
+    except Exception as e:
+        print(f"error {e}")
