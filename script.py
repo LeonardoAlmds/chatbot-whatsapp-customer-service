5bc2b12e582265899f2ffa2e876c0a0e48f50c7c
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2024-12-05 12:09:35 -0300

    Refactor WhatsApp automation: restructure main loop and enhance message handling with improved error management

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+import time
 from time import sleep
 
 import dbConfig
@@ -17,6 +18,9 @@ wait = WebDriverWait(driver, 10)
 whatsapp_url = "https://web.whatsapp.com/"
 driver.get(whatsapp_url)
 print("Scan the QR code and wait for login")
+sleep(15)
+
+coxinha = True
 
 def menu():
     input_message = wait.until(
@@ -41,9 +45,7 @@ def menu():
     input_message.send_keys(Keys.ENTER)
 
 def openUnread():
-    unreadMessage = wait.until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[3]/div[3]/div/div[2]/button[2]"))
-    )
+    unreadMessage = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[3]/div/div[2]/button[2]")
     unreadMessage.click()
 
 def seeAllProducts():
@@ -76,72 +78,80 @@ def seeAllBrands():
     
     input_message.send_keys(Keys.ENTER)
 
-c = True
-while c:
-    try:
-        openUnread()
-        c = False
-        try:      
-            input_message = wait.until(
-                EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[3]/div[4]/div/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p'))
-            )
-
-            message = "Hello, this is an automated message using Python and Selenium."
-            input_message.send_keys(message)
-            input_message.send_keys(Keys.SHIFT, Keys.ENTER)
-            input_message.send_keys(Keys.SHIFT, Keys.ENTER)
-            
-            message2 = "How can I help you today?"
-            input_message.send_keys(message2)
-            input_message.send_keys(Keys.ENTER)
-            
-            menu()
-
-            print("Messages sent successfully")
-            print("Reading received messages...")
-
-            last_message_count = len(driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text'))
-            
-            message_hour = driver.find_elements(By.CLASS_NAME, 'x1rg5ohu.x16dsc37')
-            last_hour = message_hour[-1].text
-            
-            while True:
-                sleep(5)
-
-                received_messages = driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text')
-                current_message_count = len(received_messages)
-
-                if current_message_count > last_message_count:
-                    new_messages = received_messages[last_message_count:]
-                    for message_element in new_messages:
-                        last_message = message_element.text
-                        print(f"New message received: {last_message}" + " at " + last_hour)
-
-                        if last_message == "1":
-                            print("Seeing all products...")
-                            seeAllProducts()
-                            menu()
-                        elif last_message == "2":
-                            print("Seeing all categories...")
-                            seeAllCategories()
-                            menu()
-                        elif last_message == "3":
-                            print("Seeing all brands...")
-                            seeAllBrands()
-                            menu()
-                        elif last_message == "4":
-                            print("Exiting...")
-                            break
-                        else:
-                            print("Invalid option. Try again.")
-                            menu()
-
-                    last_message_count = current_message_count
-
+def main():
+    global coxinha
+    c = True
+    while c:
+        try:
+            openUnread()
+            c = False
         except Exception as e:
-            print(f"An error occurred: {e}")
-        finally:
-            driver.quit()
-    except Exception as e:
-        print(f"An error occurred: {e}, trying again")
-        openUnread()
+            print(f"error {e}, trying again")
+            openUnread()
+    
+    while True:
+        sleep(1)
+        while coxinha:
+            sleep(2)
+            try:
+                bubbleNotifications = driver.find_elements(By.CLASS_NAME, "_ahlk")
+                for i in range(len(bubbleNotifications)):
+                    notification = bubbleNotifications[i]
+                    notification.click()
+                    
+                    print("Reading received messages...")
+
+                    last_message_count = len(driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text'))
+                    
+                    message_hour = driver.find_elements(By.CLASS_NAME, 'x1rg5ohu.x16dsc37')
+                    last_hour = message_hour[-1].text
+                    
+                    boolean = True
+                    
+                    start_time = time.time()
+                    while boolean:
+                        sleep(5)
+
+                        received_messages = driver.find_elements(By.CSS_SELECTOR, 'div.message-in span.selectable-text')
+                        current_message_count = len(received_messages)
+
+                        if current_message_count > last_message_count:
+                            new_messages = received_messages[last_message_count:]
+                            for message_element in new_messages:
+                                last_message = message_element.text
+                                print(f"New message received: {last_message}" + " at " + last_hour)
+
+                                if last_message == "1":
+                                    print("Seeing all products...")
+                                    seeAllProducts()
+                                    menu()
+                                elif last_message == "2":
+                                    print("Seeing all categories...")
+                                    seeAllCategories()
+                                    menu()
+                                elif last_message == "3":
+                                    print("Seeing all brands...")
+                                    seeAllBrands()
+                                    menu()
+                                elif last_message == "4":
+                                    print("Exiting...")
+                                    boolean = False
+                                    break
+                                else:
+                                    print("Invalid option. Try again.")
+                                    menu()
+
+                            last_message_count = current_message_count
+                            start_time = time.time()  # Reset the timer after receiving a message
+
+                        if time.time() - start_time > 30:
+                            print("No new messages received in the last 30 seconds.")
+                            boolean = False
+                    
+                    coxinha = False
+                sleep(2)
+            except Exception as e:
+                print(f"error {e}")
+        coxinha = True
+        
+main()
